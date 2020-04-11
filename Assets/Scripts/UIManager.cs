1b8f278456d31ee4f94ff76b15037313318a9b97
squeletteur <antoinemulliez@orange.fr>
2020-04-11 15:02:47 +0200

    antivirus UIManager

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager Instance;
+
+    public Animator antivirusAnimator;
+    public TMP_Text tittleAntivirusText;
+
+    public float timeShowingNotif;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void StartShowingAntivirus(string newTittleText, string newDescriptionText)
+    {
+        antivirusAnimator.gameObject.SetActive(true);
+        Invoke("StopShowingAntivirus", timeShowingNotif);
+    }
+
+    private void StopShowingAntivirus()
+    {
+        antivirusAnimator.SetTrigger("StopShowing");
+    }
+}
