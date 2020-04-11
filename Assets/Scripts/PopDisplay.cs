fd4771a0eefa6ebe085219d4170be63d3282bcd6
Cby <jorisgonc@hotmail.fr>
2020-04-11 15:02:01 +0200

    Prefab popup

--- Assets/Scripts/PopDisplay.cs
+++ Assets/Scripts/PopDisplay.cs
@@ -1,13 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class PopDisplay : MonoBehaviour
 {
-    public Pops pop;
+    private Pops pop;
+
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI descriptionText;
+    public TextMeshProUGUI annotation;
+    
+    public Image imagePop;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameManager_Error.Instance.SendPop(pop);
+
+        nameText.text = pop.name;
+        descriptionText.text = pop.description;
+        annotation.text = pop.typeErreur;
+
+        imagePop.sprite = pop.image;
     }
 }
