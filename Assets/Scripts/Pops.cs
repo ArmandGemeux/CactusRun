7243dcbca9cfa1b86d562d496826894ad5023aa6
Cby <jorisgonc@hotmail.fr>
2020-04-11 11:50:56 +0200

    Script Obj & TMP pro

---
+++ Assets/Scripts/Pops.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "new Pop", menuName = "Pop")]
+public class Pops : ScriptableObject
+{
+    public new string name;
+    public string description;
+    public string typeErreur;
+
+    public Sprite image;
+
+}
